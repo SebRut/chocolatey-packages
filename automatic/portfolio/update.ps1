33b5fe4facad957ff2cae815f40f1b8bdebe0647
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2018-05-07 07:59:24 +0200

    fix changed download path for portfolio

--- automatic/portfolio/update.ps1
+++ automatic/portfolio/update.ps1
@@ -17,10 +17,10 @@ function global:au_GetLatest {
     [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
     $download_page = Invoke-WebRequest -Uri $releases
 
-    $re  = "PortfolioPerformance-\d+\.\d+\.\d+-win32\.win32\.x86(_64)?\.zip"
+    $re  = "PortfolioPerformance-distro-\d+\.\d+\.\d+-win32\.win32\.x86(_64)?\.zip"
     $url = $download_page.links | ? href -match $re | select -First 2 -expand href
 
-    $version = $url[0] -split '-' | select -First 1 -Skip 1
+    $version = $url[0] -split '-' | select -First 1 -Skip 2
     $url_32 = $url[0]
     $url_64 = $url[1]
 
