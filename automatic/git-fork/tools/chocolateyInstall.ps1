06d5a7da0d613e177dca3bf0ad1fe3bfdbca72cd
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2018-08-08 10:35:47 +0200

    fix checksum type

--- automatic/git-fork/tools/chocolateyInstall.ps1
+++ automatic/git-fork/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@
 $packageArgs = @{
   packageName    = 'git-fork'
   url            = 'https://git-fork.com/update/win/ForkInstaller.exe'
-  checksum       = '6ffda3f17d47770869c6bc94de3e27ce906873808538c0e437283a2d6ed9ea7f'
-  checksumType   = 'sha512'
+  checksum       = '1287b648a5797fc15eebd25b67007c86368751f9821458f676409511d5a34167'
+  checksumType   = 'sha256'
   installerType  = 'exe'
   silentArgs    = '/s'
   validExitCodes = @(0)
