699b50766d9904b5ba49f2e3aed9852460e1259a
SebRut <sebastian.rutofski@gmail.com>
2019-02-22 09:44:08 +0100

    fix portfolio

--- automatic/portfolio/update.ps1
+++ automatic/portfolio/update.ps1
@@ -5,29 +5,29 @@ $releases = 'http://www.portfolio-performance.info/portfolio/'
 function global:au_SearchReplace {
     @{
         'tools\chocolateyInstall.ps1' = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"  = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*url64bit\s*=\s*)('.*')"  = "`$1'$($Latest.URL64)'"
-            "(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+            "(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
         }
-     }
+    }
 }
 
- function global:au_BeforeUpdate() {
-     $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
-	 $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
-  }
+function global:au_BeforeUpdate() {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
+}
 
 function global:au_GetLatest {
     [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
     $download_page = Invoke-WebRequest -Uri $releases
 
-    $re  = "PortfolioPerformance-distro-\d+\.\d+\.\d+-win32\.win32\.x86(_64)?\.zip"
+    $re = "PortfolioPerformance-distro-\d+\.\d+\.\d+-win32\.win32\.x86(_64)?\.zip"
     $url = $download_page.links | ? href -match $re | select -First 2 -expand href
 
     $version = $url[0] -split '-' | select -First 1 -Skip 2
-    $url_32 = $url[0] -replace '-distro', ''
-    $url_64 = $url[1] -replace '-distro', ''
+    $url_32 = $url[0]
+    $url_64 = $url[1]
 
     $Latest = @{ URL32 = $url_32; URL64 = $url_64; ChecksumType32 = 'sha256'; ChecksumType64 = 'sha256'; Version = $version }
     return $Latest
