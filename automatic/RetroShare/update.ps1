4d3ac4cbe5f10d003bbcac5d975e063adc229ef2
SebRut <sebastian.rutofski@gmail.com>
2021-06-25 22:48:43 +0200

    update retroshare release url matching

--- automatic/RetroShare/update.ps1
+++ automatic/RetroShare/update.ps1
@@ -15,13 +15,14 @@ function global:au_GetLatest {
     [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
-    $re = "RetroShare-\d+\.\d+\.\d+-\d{8}-\d*-.{8,10}-Qt-\d+\.\d+\.\d+-setup.exe"
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $re = "RetroShare-\d+\.\d+\.\d+-\d{8}-\d*-.{8,10}-Qt-\d+\.\d+\.\d+-(x86|x64)-setup.exe"
+    $url = $download_page.links | ? href -match $re | select -First 2 -expand href
 
-    $version = $url -split '-' | select -First 1 -Skip 1
-    $url = 'https://github.com' + $url
+    $version = $url[0] -split '-' | select -First 1 -Skip 1
+    $url_64_bit = 'https://github.com' + $url[0]
+    $url_32_bit = 'https://github.com' + $url[1]
 
-    $Latest = @{ URL32 = $url; Version = $version }
+    $Latest = @{ URL32 = $url_32_bit; URL64 = $url_64_bit; Version = $version }
     return $Latest
 }
 
